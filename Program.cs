8910d7bb319399e5caccf6e1442dd04001e63c4d
bruk202 <40613642+bruk202@users.noreply.github.com>
2022-06-03 13:07:33 -0400

    IsPrime

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+
+class prime
+{
+	static bool isPrime(int num)
+	{
+
+
+		if (num % 2 == 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	static void Main()
+	{
+		int x = Int32.Parse(Console.ReadLine());
+		if (isPrime(x))
+		{
+			Console.WriteLine("true");
+		}
+		else
+		{
+			Console.WriteLine("false");
+		}
+		Thread.Sleep(20000);
+	}
+}
